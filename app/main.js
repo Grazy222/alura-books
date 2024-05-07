c18a0dd0db74e6e899a887b8ac0172d90c2fc4a1
Grazy222 <graciele222@gmail.com.br>
2024-05-07 13:08:07 -0300

    Iniciando projeto de livros

---
+++ app/main.js
@@ -0,0 +1,16 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+// const elementoParainserirLivros = document.getElementById('livros')
+//inserindo livros que está dentro section index-html
+
+
+// função buscar livros API asyncromi
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI) //realizando requisição como promessa
+    livros = await res.json()
+    let livrosComDescontos = aplicarDesconto(livros) //aplicando descontos nos livros
+    exibirOsLivrosNaTela(livrosComDescontos) 
+
+}
